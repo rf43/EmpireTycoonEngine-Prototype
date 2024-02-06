6e7760708ef2fd638aace24979d81f9f91b39449
Rich Friedel <rich.friedel@gmail.com>
2024-02-05 21:24:21 -0500

    Tweaking the idle, walk, run

--- EmpireTycoonEngine-Prototype-v1/Assets/EmpireTycoonEngine/Scripts/NavAgentMovement.cs
+++ EmpireTycoonEngine-Prototype-v1/Assets/EmpireTycoonEngine/Scripts/NavAgentMovement.cs
@@ -1,29 +1,26 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
 namespace IvyCreek.EmpireTycoonEngine.Scripts
 {
     [AddComponentMenu(menuName: "EmpireTycoonEngine/Scripts/NavAgentMovement")]
+    [RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
     public class NavAgentMovement : MonoBehaviour
     {
-        [SerializeField] private TargetPositioner _target;
         [SerializeField] private float _walkSpeed = 1.5f;
         [SerializeField] private float _runSpeed = 1.5f;
         [SerializeField] private float _minDistance = 2f;
         [SerializeField] private float _runDistance = 10f;
-        [SerializeField, Range(1, 10)] private int _waitForMinSeconds;
+        [SerializeField, Range(0, 10)] private int _waitForMinSeconds;
         [SerializeField, Range(1, 10)] private int _waitForMaxSeconds;
 
         private NavMeshAgent _navMeshAgent;
         private Animator _animator;
         
-        static readonly int Speed = Animator.StringToHash("Speed");
+        private static readonly int Speed = Animator.StringToHash("Speed");
         
-        public static event UnityAction OnTargetReached = delegate { }; 
 
         private void Awake()
         {
@@ -36,39 +33,51 @@ namespace IvyCreek.EmpireTycoonEngine.Scripts
             StartCoroutine(TargetWanderAround());
         }
 
+        private const float MinRange = -30f;
+        private const float MaxRange = 10f;
+        private const float ZAxisMinRange = -11f;
+        private const float ZAxisMaxRange = 11f;
+
         private IEnumerator TargetWanderAround()
         {
             while (true)
             {
                 if (_navMeshAgent.remainingDistance < _minDistance)
                 {
-                    Debug.Log($"Distance => {_navMeshAgent.remainingDistance}");
-                    Debug.Log("IDLE");
-                    _animator.SetFloat(Speed, 0);
-                    OnTargetReached?.Invoke();
+                    var animationSpeed = Random.Range(0f, 0.3f);
+                    var walkSpeed = animationSpeed < 0.02f ? 0 : Random.Range(0f, 0.1f);
+                    SetAgentSpeedAnimation(walkSpeed, animationSpeed);
+
+                    var randomPosition = GenerateRandomPosition();
                     yield return new WaitForSeconds(Random.Range(_waitForMinSeconds, _waitForMaxSeconds));
-                    _navMeshAgent.SetDestination(_target.transform.position);
+
+                    _navMeshAgent.SetDestination(randomPosition);
                     yield return null;
                 }
-                else if(_navMeshAgent.remainingDistance > _runDistance)
+                else if (_navMeshAgent.remainingDistance > _runDistance)
                 {
-                    Debug.Log($"Distance => {_navMeshAgent.remainingDistance}");
-                    Debug.Log("RUNNING");
-                    _navMeshAgent.speed = _runSpeed;
-                    _animator.SetFloat(Speed, 1);
+                    SetAgentSpeedAnimation(_runSpeed, 1f);
                     yield return null;
                 }
                 else
                 {
-                    Debug.Log($"Distance => {_navMeshAgent.remainingDistance}");
-                    Debug.Log("WALKING");
-                    _navMeshAgent.speed = _walkSpeed;
-                    _animator.SetFloat(Speed, 0.5f);
+                    SetAgentSpeedAnimation(_walkSpeed, 0.5f);
                     yield return null;
                 }
-                
+
                 yield return null;
             }
+        }
+
+        private void SetAgentSpeedAnimation(float speed, float animationSpeed)
+        {
+            _navMeshAgent.speed = speed;
+            _animator.SetFloat(Speed, animationSpeed);
+        }
+
+        private Vector3 GenerateRandomPosition()
+        {
+            return new Vector3(Random.Range(MinRange, MaxRange), 0, Random.Range(ZAxisMinRange, ZAxisMaxRange));
         }
     }
 }
